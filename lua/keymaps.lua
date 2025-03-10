dc5c956da9452824242e9757b1733277b16c93d5
Septian Rifano <septianinet@gmail.com>
2025-03-11 01:02:31 +0700

    fix: change to built in neotree

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -45,4 +45,5 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+vim.keymap.set('n', '\\', ':Neotree filesystem reveal left<CR>', { silent = true, noremap = true })
 -- vim: ts=2 sts=2 sw=2 et
