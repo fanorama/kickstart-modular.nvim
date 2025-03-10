89afbadbff035c16dabf1370e4ee70d2367fc8b9
Septian Rifano <septianinet@gmail.com>
2025-03-11 03:49:20 +0700

    feat: change formatter for react

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -1,16 +1,16 @@
 return {
   { -- Autoformat
-    'stevearc/conform.nvim',
-    event = { 'BufWritePre' },
-    cmd = { 'ConformInfo' },
+    "stevearc/conform.nvim",
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
     keys = {
       {
-        '<leader>f',
+        "<leader>f",
         function()
-          require('conform').format { async = true, lsp_format = 'fallback' }
+          require("conform").format { async = true, lsp_format = "fallback" }
         end,
-        mode = '',
-        desc = '[F]ormat buffer',
+        mode = "",
+        desc = "[F]ormat buffer",
       },
     },
     opts = {
@@ -22,9 +22,9 @@ return {
         local disable_filetypes = { c = true, cpp = true }
         local lsp_format_opt
         if disable_filetypes[vim.bo[bufnr].filetype] then
-          lsp_format_opt = 'never'
+          lsp_format_opt = "never"
         else
-          lsp_format_opt = 'fallback'
+          lsp_format_opt = "fallback"
         end
         return {
           timeout_ms = 500,
@@ -37,12 +37,15 @@ return {
         },
       },
       formatters_by_ft = {
-        lua = { 'stylua' },
+        lua = { "stylua" },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        javascript = { "eslint_d", "prettierd", stop_after_first = true },
+        typescript = { "eslint_d", "prettierd", stop_after_first = true },
+        typescriptreact = { "eslint_d", "prettierd", stop_after_first = true },
+        javascriptreact = { "eslint_d", "prettierd", stop_after_first = true },
       },
     },
   },
